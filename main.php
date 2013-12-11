5f1dcd41bc743630ddb10774ffc5620a71050111
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-12-11 18:03:37 +0100

    Now we have omonomimies up to 3 instances per name

--- main.php
+++ main.php
@@ -7,12 +7,19 @@ Bootstrap::boot();
 $a = Csv::csv2array('example.csv');
 
 foreach ($a as $k => $member) {
-    if (! Member::where('email', $member[2])->count() AND Member::where('login', $member[3])->count()) {
+    if (Member::where('email', $member[2])->count() == 0 AND Member::where('identification', $member[3])->count() == 0) {
         $invite = sha1($member[0] . " " . $member[1] . ", " . date('c'));
         if (! Member::where('invite_code', $invite)->count()) {
             $m = new Member;
             $m->invite_code = $invite;
-            $m->login = $member[3];
+            $login = substr($member[3], 0, strpos($member[3], "@"));
+            if (Member::where('login', $login)->count() == 0)
+                $m->login = $login;
+            elseif (Member::where('login', $login + 1)->count() == 0)
+                $m->login = $login + '1';
+            elseif (Member::where('login', $login + 2)->count() == 0)
+                $m->login = $login + '2';
+            $m->identification = $member[3];
             $m->name = ucwords(strtolower($member[0])) + " " + ucwords(strtolower($member[1]));
             $m->save();
         } else {
